4941e63797362994aa5842176868ac66a492b0a0
Allan Panchi <avpanchi@espe.edu.ec>
2024-06-01 20:38:23 -0500

    Creación del metodo actualiar producto

--- productos/urls.py
+++ productos/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # URLs de vistas normales
     path('', views.listar_productos, name='listar_productos'),
     path('/create', views.crear_productos, name='crear_productos'),
+    path('update/<int:id>/', views.actualizar_productos, name='actualizar_productos'),
     path('delete', views.delete_productos, name='delete_productos')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
@@ -11,7 +11,6 @@ def listar_productos(request):
 def crear_productos(request):
     # Crea un nuevo producto
     # Guarda el producto en la base de datos
-    # Renderiza la plantilla listar.html
     if request.method == "POST":
         nombre = request.POST.get("nombre")
         precio = request.POST.get("precio")
@@ -22,7 +21,18 @@ def crear_productos(request):
         return redirect('listar_productos')
     return render(request, 'crear.html')
 
-#def actualizar_productos(request):
+def actualizar_productos(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    
+    if request.method == "POST":
+        producto.nombre = request.POST.get("nombre")
+        producto.precio = request.POST.get("precio")
+        producto.cantidad = request.POST.get("cantidad")
+        producto.save()
+        return redirect('listar_productos')
+    
+    return render(request, 'actualizar.html', {'producto': producto})
+
 
 # función para eliminar productos de la base de datos atraves de un formulario
 @csrf_exempt
