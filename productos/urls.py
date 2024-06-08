fd9f0f6cd0af6dedc48a0e474d51e46a40ceb8ff
Alejo Andrade <02alejo20@gmail.com>
2024-06-07 22:39:08 -0500

    Actualzación (Exportar e Importar csv)

--- productos/urls.py
+++ productos/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     # URLs de vistas normales
     path('', views.listar_productos, name='listar_productos'),
-    path('/create', views.crear_productos, name='crear_productos'),
+    path('create', views.crear_productos, name='crear_productos'),
+    path('import', views.importar_productos, name='importar_productos'),
     path('update/<int:id>/', views.actualizar_productos, name='actualizar_productos'),
-    path('delete', views.delete_productos, name='delete_productos')
+    path('delete', views.delete_productos, name='delete_productos'),
+    path('export', views.export, name='export')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.core.files import File
+import csv
+from django.http import HttpResponse
 
 def listar_productos(request):
     # Consulta a la base de datos
     # Renderiza la plantilla listar.html
     productos = Producto.objects.all()
     return render(request, 'listar.html', {'productos': productos})
+
+def export(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="data.csv"'
+    
+    writer = csv.writer(response)
+    productos = Producto.objects.all()
+    for prod in productos:
+        writer.writerow([prod.nombre, prod.precio, prod.cantidad])
+    
+    return response
+
+# función para importar productos de un archivo csv
+def importar_productos(request):
+    if request.method == "POST":
+        archivo = request.FILES["file"]
+        for linea in archivo:
+            linea = linea.decode("utf-8")
+            nombre, precio, cantidad = linea.split(",")
+            Producto.objects.create(nombre=nombre, precio=precio, cantidad=cantidad)
+        return redirect('listar_productos')
+    return render(request, 'crear.html')
 
 def crear_productos(request):
     # Crea un nuevo producto
@@ -35,11 +60,11 @@ def actualizar_productos(request, id):
 
 
 # función para eliminar productos de la base de datos atraves de un formulario
-@csrf_exempt
 def delete_productos(request):
     productos = Producto.objects.all()
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        producto = Producto.objects.get(nombre=nombre)
+        id = request.POST.get('id')
+        producto = Producto.objects.get(id=id)
         producto.delete()
-    return render(request, 'delete.html', {'productos': productos})
+        return redirect('listar_productos')
+    return render(request, 'listar.html', {'productos': productos})
